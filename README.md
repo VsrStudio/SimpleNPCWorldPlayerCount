5b444113ce31a25a2e603b298d1f720945b9b87f
brokiem <milyhqvideo@gmail.com>
2021-04-19 07:19:51 +0800

    compatible with SimpleNPC

--- README.md
+++ README.md
@@ -1,10 +1,5 @@
-# WorldPlayerCount [![Poggit-CI](https://poggit.pmmp.io/ci.badge/xXKHaLeD098Xx/WorldPlayerCount/WorldPlayerCount)](https://poggit.pmmp.io/ci/xXKHaLeD098Xx/WorldPlayerCount/WorldPlayerCount)
-
-[![](https://poggit.pmmp.io/shield.dl.total/WorldPlayerCount)](https://poggit.pmmp.io/p/WorldPlayerCount)
-[![](https://poggit.pmmp.io/shield.state/WorldPlayerCount)](https://poggit.pmmp.io/p/WorldPlayerCount)
-[![Poggit](https://poggit.pmmp.io/ci.shield/xXKHaLeD098Xx/WorldPlayerCount/WorldPlayerCount?style=flat-square)](https://poggit.pmmp.io/ci/xXKHaLeD098Xx/WorldPlayerCount/WorldPlayerCount)
-<br>
-A simple PocketMine addon plugin for Slapper which allows you to create a slapper counting the players of a world(s) on its name tag.<br>
+# WorldPlayerCount 
+A simple PocketMine addon plugin for SimpleNPC which allows you to create a SimpleNPC counting the players of a world(s) on its name tag.<br>
 ## Versions
 - v1.0-beta
   - First version
@@ -13,17 +8,17 @@ A simple PocketMine addon plugin for Slapper which allows you to create a slappe
   - Added combined world player count support!
   - Added customizable count-check interval, see the config
 ## Usage for single world
-__Note: This is used when creating a slapper counting the players of 1 world only__
+__Note: This is used when creating a snpc counting the players of 1 world only__
 - First thing we add after the entity type (human) is the nametag we want, followed by a {line} tag then adding "count WORLDNAME"<br><br>
-- __Example : /slapper spawn human Hub{line}count Hub<br>__<br>
+- __Example : /snpc spawn human Hub{line}count Hub<br>__<br>
 - Congrats!, you spawned an entity counting the players of the world "Hub"
 ## Usage for combined worlds
-__Note: This is used when creating a slapper counting the players of more than 1 world__
+__Note: This is used when creating a snpc counting the players of more than 1 world__
 - First thing we add after the entity type (human) is the nametag we want, followed by a {line} tag then adding "combinedcounts World1&World2" and so on with the "&" symbol<br><br>
-- __Example : /slapper spawn human SkyWars{line}combinedcounts SK-1&SK-2&SK-3__<br><br>
+- __Example : /snpc spawn human SkyWars{line}combinedcounts SK-1&SK-2&SK-3__<br><br>
 - Congrats!, you spawned an entity counting the players of the worlds "SK-1", "SK-2" and "SK-3" at the same time
 ## Config
-- In the config you can customize the name tag of the slapper, it is set to "{number} Playing" by default<br>
+- In the config you can customize the name tag of the snpc, it is set to "{number} Playing" by default<br>
 - The count "check" interval is also customizable and it is set to 1 second by default
 ## Contacts
 In case you are confused about the usage or found a bug please contact me on my discord __@кнαℓє∂#7787__
--- src/xXKHaLeD098Xx/WorldPlayerCount/WorldPlayerCount.php
+++ src/xXKHaLeD098Xx/WorldPlayerCount/WorldPlayerCount.php
@@ -6,219 +6,225 @@
 
 namespace xXKHaLeD098Xx\WorldPlayerCount;
 
+use brokiem\snpc\entity\BaseNPC;
+use brokiem\snpc\entity\CustomHuman;
+use brokiem\snpc\event\SNPCCreationEvent;
+use brokiem\snpc\event\SNPCDeletionEvent;
+use brokiem\snpc\SimpleNPC;
+use pocketmine\event\entity\EntityDamageByEntityEvent;
 use pocketmine\event\Listener;
+use pocketmine\plugin\Plugin;
 use pocketmine\plugin\PluginBase;
-use slapper\events\SlapperCreationEvent;
-use slapper\events\SlapperDeletionEvent;
 use xXKHaLeD098Xx\WorldPlayerCount\Task\RefreshCount;
-use slapper\events\SlapperHitEvent;
-use slapper\Main as SlapperMain;
 use pocketmine\Player;
 
-class WorldPlayerCount extends PluginBase implements Listener{
+class WorldPlayerCount extends PluginBase implements Listener {
 
-	public function getSlapper() : SlapperMain{
-		/** @var SlapperMain $api */
-		$api = $this->getServer()->getPluginManager()->getPlugin("Slapper");
-		return $api;
-	}
+    /** @return SimpleNPC */
+    public function getSlapper(): Plugin{
+        return $this->getServer()->getPluginManager()->getPlugin("SimpleNPC");
+    }
 
-	public function onEnable()
-	{
-		$map = $this->getDescription()->getMap();
-		$ver = $this->getDescription()->getVersion();
-		if(isset($map["author"])){
-			if($map["author"] !== "xXKHaLeD098Xx" or $ver !== "2.0-beta"){
-				$this->getLogger()->emergency("§cPlugin info has been changed, please give the author the proper credits, set the author to \"xXKHaLeD098Xx\" and setting the version to \"2.0-beta\" if required, or else the server will shutdown on every start-up");
-				$this->getServer()->shutdown();
-				return;
-			}
-		} else {
-			$this->getLogger()->emergency("§cPlugin info has been changed, please give the author the proper credits, set the author to \"xXKHaLeD098Xx\" and setting the version to \"2.0-beta\" if required, or else the server will shutdown on every start-up");
-			$this->getServer()->shutdown();
-		}
-		$this->getServer()->getPluginManager()->registerEvents($this, $this);
-		$this->saveDefaultConfig();
-		$this->saveResource("config.yml");
-		$this->getScheduler()->scheduleRepeatingTask(new RefreshCount($this), (int) $this->getConfig()->get("count-interval") * 20);
-		$worlds = $this->getConfig()->get("worlds");
-		foreach ($worlds as $key => $world){
-			if (file_exists($this->getServer()->getDataPath()."/worlds/".$world)){
-				$this->getServer()->loadLevel($world);
-			} else {
-				unset($worlds[$key]);
-				$this->getConfig()->set("worlds", $worlds);
-				$this->getConfig()->save();
-			}
-		}
-	}
+    public function onEnable(): void{
+        $map = $this->getDescription()->getMap();
+        $ver = $this->getDescription()->getVersion();
+        if(isset($map["author"])){
+            if($map["author"] !== "xXKHaLeD098Xx" or $ver !== "2.0-beta"){
+                $this->getLogger()->emergency("§cPlugin info has been changed, please give the author the proper credits, set the author to \"xXKHaLeD098Xx\" and setting the version to \"2.0-beta\" if required, or else the server will shutdown on every start-up");
+                $this->getServer()->shutdown();
+                return;
+            }
+        }else{
+            $this->getLogger()->emergency("§cPlugin info has been changed, please give the author the proper credits, set the author to \"xXKHaLeD098Xx\" and setting the version to \"2.0-beta\" if required, or else the server will shutdown on every start-up");
+            $this->getServer()->shutdown();
+        }
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+        $this->saveDefaultConfig();
+        $this->saveResource("config.yml");
+        $this->getScheduler()->scheduleRepeatingTask(new RefreshCount($this), (int)$this->getConfig()->get("count-interval") * 20);
+        $worlds = $this->getConfig()->get("worlds");
+        foreach($worlds as $key => $world){
+            if(file_exists($this->getServer()->getDataPath() . "/worlds/" . $world)){
+                $this->getServer()->loadLevel($world);
+            }else{
+                unset($worlds[$key]);
+                $this->getConfig()->set("worlds", $worlds);
+                $this->getConfig()->save();
+            }
+        }
+    }
 
-	public function slapperCreation(SlapperCreationEvent $ev){
-		$entity = $ev->getEntity();
-		$name = $entity->getNameTag();
-		if(strpos($name, "\n") !== false){
-			$allines = explode("\n", $name);
-			$pos = strpos($allines[1], "count ");
-			if($pos !== false){
-				//Single world
-				$levelname = str_replace("count ", "", $allines[1]);
-				if(file_exists($this->getServer()->getDataPath()."/worlds/".$levelname)){
-					if (!$this->getServer()->isLevelLoaded($levelname)) $this->getServer()->loadLevel($levelname);
-					$entity->namedtag->setString("playerCount", $levelname);
-					$worlds = $this->getConfig()->get("worlds");
-					if(!in_array($levelname, $worlds)){
-						array_push($worlds, $levelname);
-						$this->getConfig()->set("worlds", $worlds);
-						$this->getConfig()->save();
-						return;
-					}
-				}
-			}
-			//Multi-world
-			$combinedPos = strpos($allines[1], "combinedcounts ");
-			if($combinedPos !== false){
-				$symbolPos = strpos($allines[1], "&");
-				if($symbolPos !== false){
-					$levelnameS = str_replace("combinedcounts ", "", $allines[1]);
-					$levelnamesInArray = explode("&", $levelnameS);
-					if(in_array("", $levelnamesInArray)) return;
-					$entity->namedtag->setString("combinedPlayerCounts", $levelnameS);
-					$this->combinedPlayerCounts();
-				}
-			}
-		}
-	}
+    public function slapperCreation(SNPCCreationEvent $ev): void{
+        $entity = $ev->getEntity();
+        $name = $entity->getNameTag();
+        if(strpos($name, "\n") !== false){
+            $allines = explode("\n", $name);
+            $pos = strpos($allines[1], "count ");
+            if($pos !== false){
+                //Single world
+                $levelname = str_replace("count ", "", $allines[1]);
+                if(file_exists($this->getServer()->getDataPath() . "/worlds/" . $levelname)){
+                    if(!$this->getServer()->isLevelLoaded($levelname)){
+                        $this->getServer()->loadLevel($levelname);
+                    }
+                    $entity->namedtag->setString("playerCount", $levelname);
+                    $worlds = $this->getConfig()->get("worlds");
+                    if(!in_array($levelname, $worlds, true)){
+                        $worlds[] = $levelname;
+                        $this->getConfig()->set("worlds", $worlds);
+                        $this->getConfig()->save();
+                        return;
+                    }
+                }
+            }
+            //Multi-world
+            $combinedPos = strpos($allines[1], "combinedcounts ");
+            if($combinedPos !== false){
+                $symbolPos = strpos($allines[1], "&");
+                if($symbolPos !== false){
+                    $levelnameS = str_replace("combinedcounts ", "", $allines[1]);
+                    $levelnamesInArray = explode("&", $levelnameS);
+                    if(in_array("", $levelnamesInArray, true)){
+                        return;
+                    }
+                    $entity->namedtag->setString("combinedPlayerCounts", $levelnameS);
+                    $this->combinedPlayerCounts();
+                }
+            }
+        }
+    }
 
-	public function onSlapperDeletion(SlapperDeletionEvent $event){
+    public function onSlapperDeletion(SNPCDeletionEvent $event): void{
 
-		// single world
-		if($event->getEntity()->namedtag->hasTag("playerCount")){
-			$tag = $event->getEntity()->namedtag->getString("playerCount");
-			$event->getEntity()->namedtag->removeTag("playerCount");
-			$worlds = $this->getConfig()->get("worlds");
-			unset($worlds[array_search($tag, $worlds)]);
-			$this->getConfig()->set("worlds", $worlds);
-			$this->getConfig()->save();
-		}
-		// combined
-		if($event->getEntity()->namedtag->hasTag("combinedPlayerCounts")){
-			$tag = $event->getEntity()->namedtag->getString("combinedPlayerCounts");
-			$event->getEntity()->namedtag->removeTag("combinedPlayerCounts");
-			$worlds = $this->getConfig()->get("worlds");
-			$arrayOfNames = explode("&", $tag);
-			foreach ($arrayOfNames as $name){
-				if(in_array($name, $worlds)){
-					unset($worlds[array_search($name, $worlds)]);
-			        $this->getConfig()->set("worlds", $worlds);
-			        $this->getConfig()->save();
-				}
-			}
-		}
-	}
+        // single world
+        if($event->getEntity()->namedtag->hasTag("playerCount")){
+            $tag = $event->getEntity()->namedtag->getString("playerCount");
+            $event->getEntity()->namedtag->removeTag("playerCount");
+            $worlds = $this->getConfig()->get("worlds");
+            unset($worlds[array_search($tag, $worlds, true)]);
+            $this->getConfig()->set("worlds", $worlds);
+            $this->getConfig()->save();
+        }
+        // combined
+        if($event->getEntity()->namedtag->hasTag("combinedPlayerCounts")){
+            $tag = $event->getEntity()->namedtag->getString("combinedPlayerCounts");
+            $event->getEntity()->namedtag->removeTag("combinedPlayerCounts");
+            $worlds = $this->getConfig()->get("worlds");
+            $arrayOfNames = explode("&", $tag);
+            foreach($arrayOfNames as $name){
+                if(in_array($name, $worlds, true)){
+                    unset($worlds[array_search($name, $worlds, true)]);
+                    $this->getConfig()->set("worlds", $worlds);
+                    $this->getConfig()->save();
+                }
+            }
+        }
+    }
 
-	public function onSlapperHit(SlapperHitEvent $event){ // jojoe77777 didn't call SlapperDeletionEvent on EntityDamageEvent so i will do it instead
-		$slapper = $this->getSlapper();
-		$damager = $event->getDamager();
-		if($damager instanceof Player){
-			if(isset($slapper->hitSessions[$damager->getName()])){
-				$slapperDelete = new SlapperDeletionEvent($event->getEntity());
-				$slapperDelete->call();
-			}
-		}
-	}
+    public function DamageEvent(EntityDamageByEntityEvent $event): void{
+        $damager = $event->getDamager();
+        $entity = $event->getEntity();
 
-	// here no single world allowed, only combined ones
+        if(($entity instanceof BaseNPC || $entity instanceof CustomHuman) and $damager instanceof Player){
+            if(isset($slapper->hitSessions[$damager->getName()])){
+                $slapperDelete = new SNPCDeletionEvent($event->getEntity());
+                $slapperDelete->call();
+            }
+        }
+    }
 
-	public function combinedPlayerCounts(){
-		$levels = $this->getServer()->getLevels();
-		foreach ($levels as $level){
-			foreach($level->getEntities() as $entity){
-				$nbt = $entity->namedtag;
-				if($nbt->hasTag("combinedPlayerCounts") && !$nbt->hasTag("playerCount")){
-					$worldsNames = explode("&", $nbt->getString("combinedPlayerCounts"));
-					foreach ($worldsNames as $name){
-						if(!file_exists($this->getServer()->getDataPath()."/worlds/".$name)){
-							unset($worldsNames[array_search($name, $worldsNames)]);
-							$slapperDelete = new SlapperDeletionEvent($entity);
-							$slapperDelete->call();
-							$entity->close();
-						}
-					}
-					// extra checks just in case
-					if(count($worldsNames) > 1){
-						$counts = 0;
-						foreach ($worldsNames as $name){
-							if($name === ""){
-								continue;
-							}
-							if($this->getServer()->isLevelLoaded($name)){
-								$worlds = $this->getConfig()->get("worlds");
-								if(!in_array($name, $worlds)){
-									$worlds[] = $name;
-									$this->getConfig()->set("worlds", $worlds);
-									$this->getConfig()->save();
-								}
-								$pmLevel = $this->getServer()->getLevelByName($name);
-								$countOfLevel = count($pmLevel->getPlayers());
-								$counts += $countOfLevel;
-							} else {
-								$worlds = $this->getConfig()->get("worlds");
-								if(!in_array($name, $worlds)){
-									$worlds[] = $name;
-									$this->getConfig()->set("worlds", $worlds);
-									$this->getConfig()->save();
-								}
-								$this->getServer()->loadLevel($name);
-							}
-						}
-						$count = $this->getConfig()->get("count");
-						$str = str_replace("{number}", $counts, $count);
-						$allines = explode("\n", $entity->getNameTag());
-						$entity->setNameTag($allines[0]."\n".$str);
-					}
-				}
-			}
-		}
-	}
+    // here no single world allowed, only combined ones
 
-	public function playerCount(){
-		$levels = $this->getServer()->getLevels();
-		foreach ($levels as $level){
-			$entities = $level->getEntities();
-			foreach ($entities as $entity){
-				$nbt = $entity->namedtag;
-				if($nbt->hasTag("playerCount") && !$nbt->hasTag("combinedPlayerCounts")){
-					$world = $nbt->getString("playerCount");
-					$allines = explode("\n", $entity->getNameTag());
-					if(file_exists($this->getServer()->getDataPath()."/worlds/".$world)){
-						if($this->getServer()->isLevelLoaded($world)){
-							$worlds = $this->getConfig()->get("worlds");
-							if(!in_array($world, $worlds)){
-								$worlds[] = $world;
-								$this->getConfig()->set("worlds", $worlds);
-								$this->getConfig()->save();
-							}
-							$countPlayers = count($this->getServer()->getLevelByName($world)->getPlayers());
-							$count = $this->getConfig()->get("count");
-							$str = str_replace("{number}", $countPlayers, $count);
-							$entity->setNameTag($allines[0]."\n".$str);
-						} else {
-							$worlds = $this->getConfig()->get("worlds");
-							if(!in_array($world, $worlds)){
-								$worlds[] = $world;
-								$this->getConfig()->set("worlds", $worlds);
-								$this->getConfig()->save();
-							}
-							$this->getServer()->loadLevel($world);
-						}
-					} else {
-						$slapperDelete = new SlapperDeletionEvent($entity);
-						$slapperDelete->call();
-						$entity->close();
-					}
-				}
-			}
-		}
-	}
+    public function combinedPlayerCounts(): void{
+        $levels = $this->getServer()->getLevels();
+        foreach($levels as $level){
+            foreach($level->getEntities() as $entity){
+                $nbt = $entity->namedtag;
+                if($nbt->hasTag("combinedPlayerCounts") && !$nbt->hasTag("playerCount")){
+                    $worldsNames = explode("&", $nbt->getString("combinedPlayerCounts"));
+                    foreach($worldsNames as $name){
+                        if(!file_exists($this->getServer()->getDataPath() . "/worlds/" . $name)){
+                            unset($worldsNames[array_search($name, $worldsNames, true)]);
+                            $slapperDelete = new SNPCDeletionEvent($entity);
+                            $slapperDelete->call();
+                            $entity->close();
+                        }
+                    }
+                    // extra checks just in case
+                    if(count($worldsNames) > 1){
+                        $counts = 0;
+                        foreach($worldsNames as $name){
+                            if($name === ""){
+                                continue;
+                            }
+                            if($this->getServer()->isLevelLoaded($name)){
+                                $worlds = $this->getConfig()->get("worlds");
+                                if(!in_array($name, $worlds, true)){
+                                    $worlds[] = $name;
+                                    $this->getConfig()->set("worlds", $worlds);
+                                    $this->getConfig()->save();
+                                }
+                                $pmLevel = $this->getServer()->getLevelByName($name);
+                                $countOfLevel = count($pmLevel->getPlayers());
+                                $counts += $countOfLevel;
+                            }else{
+                                $worlds = $this->getConfig()->get("worlds");
+                                if(!in_array($name, $worlds, true)){
+                                    $worlds[] = $name;
+                                    $this->getConfig()->set("worlds", $worlds);
+                                    $this->getConfig()->save();
+                                }
+                                $this->getServer()->loadLevel($name);
+                            }
+                        }
+                        $count = $this->getConfig()->get("count");
+                        $str = str_replace("{number}", $counts, $count);
+                        $allines = explode("\n", $entity->getNameTag());
+                        $entity->setNameTag($allines[0] . "\n" . $str);
+                    }
+                }
+            }
+        }
+    }
+
+    public function playerCount(): void{
+        $levels = $this->getServer()->getLevels();
+        foreach($levels as $level){
+            $entities = $level->getEntities();
+            foreach($entities as $entity){
+                $nbt = $entity->namedtag;
+                if($nbt->hasTag("playerCount") && !$nbt->hasTag("combinedPlayerCounts")){
+                    $world = $nbt->getString("playerCount");
+                    $allines = explode("\n", $entity->getNameTag());
+                    if(file_exists($this->getServer()->getDataPath() . "/worlds/" . $world)){
+                        if($this->getServer()->isLevelLoaded($world)){
+                            $worlds = $this->getConfig()->get("worlds");
+                            if(!in_array($world, $worlds, true)){
+                                $worlds[] = $world;
+                                $this->getConfig()->set("worlds", $worlds);
+                                $this->getConfig()->save();
+                            }
+                            $countPlayers = count($this->getServer()->getLevelByName($world)->getPlayers());
+                            $count = $this->getConfig()->get("count");
+                            $str = str_replace("{number}", $countPlayers, $count);
+                            $entity->setNameTag($allines[0] . "\n" . $str);
+                        }else{
+                            $worlds = $this->getConfig()->get("worlds");
+                            if(!in_array($world, $worlds, true)){
+                                $worlds[] = $world;
+                                $this->getConfig()->set("worlds", $worlds);
+                                $this->getConfig()->save();
+                            }
+                            $this->getServer()->loadLevel($world);
+                        }
+                    }else{
+                        $slapperDelete = new SNPCDeletionEvent($entity);
+                        $slapperDelete->call();
+                        $entity->close();
+                    }
+                }
+            }
+        }
+    }
 }
 
