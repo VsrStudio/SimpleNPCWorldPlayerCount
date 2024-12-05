fd33755355737e5eac118f50f12eef7a33e5ecd0
XOneFG <117354752+VsrStudio@users.noreply.github.com>
2024-12-05 07:52:10 +0700

    Update RefreshCount.php

--- src/xXKHaLeD098Xx/WorldPlayerCount/Task/RefreshCount.php
+++ src/xXKHaLeD098Xx/WorldPlayerCount/Task/RefreshCount.php
@@ -1,19 +1,21 @@
 <?php
+
 namespace xXKHaLeD098Xx\WorldPlayerCount\Task;
 
 use pocketmine\scheduler\Task;
 use xXKHaLeD098Xx\WorldPlayerCount\WorldPlayerCount;
 
 class RefreshCount extends Task {
-	private $plugin;
-	public function __construct(WorldPlayerCount $plugin)
-	{
-		$this->plugin = $plugin;
-	}
 
-	public function onRun(int $currentTick)
-	{
-		$this->plugin->playerCount();
-		$this->plugin->combinedPlayerCounts();
-	}
+    /** @var WorldPlayerCount */
+    private $plugin;
+
+    public function __construct(WorldPlayerCount $plugin) {
+        $this->plugin = $plugin;
+    }
+
+    public function onRun(int $currentTick): void {
+        $this->plugin->playerCount();
+        $this->plugin->combinedPlayerCounts();
+    }
 }
